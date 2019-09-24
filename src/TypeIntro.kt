c7920d7c61c5dd8398179ecfce5031bdece3fae3
MarioJC <mario.rol@gmail.com>
2019-09-24 16:48:51 +0200

    Challenge: The Unicorn's Horn

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -3,9 +3,14 @@ const val MAX_EXPERIENCE: Int = 5000
 fun main(args: Array<String>) {
     val playerName = "Estragon"
     var experiencePoints = 5
+    experiencePoints += 5
+
     var hasSteed = false
 
-    experiencePoints += 5
+    val pubName = "Unicorn's Horn"
+    var publicanName = "John Doe"
+    var goldPieces = 50
+    val drinks = listOf("mead", "wine", "LaCroix")
 
     println(playerName)
     println(experiencePoints)
