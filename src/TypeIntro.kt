ca73a91f05698828e1d6e267c24559f59e873799
MarioJC <mario.rol@gmail.com>
2019-09-24 16:44:45 +0200

    Challenge: hasSteed

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -3,6 +3,7 @@ const val MAX_EXPERIENCE: Int = 5000
 fun main(args: Array<String>) {
     val playerName = "Estragon"
     var experiencePoints = 5
+    var hasSteed = false
 
     experiencePoints += 5
 
