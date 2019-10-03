5bdacd2733e85a386740aa41b9fbe85ca98007b3
MarioJC <mario.rol@gmail.com>
2019-10-03 16:31:18 +0200

    Function References
    Lambdas can be passed around but so can named functions!
    Introduces the :: operator

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,12 +1,18 @@
 fun main() {
-    runSimulation("Guyal") { playerName, numBuildings ->
+    runSimulation("Guyal", ::printContructionCost) { playerName, numBuildings ->
         val currentYear = 2019
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..3).shuffled().last() // randomly selects 1, 2, or 3
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printContructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
