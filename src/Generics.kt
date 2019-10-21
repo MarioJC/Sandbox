4734b771b5841ca44caf6e33cc45d14d255ee8ce
MarioJC <mario.rol@gmail.com>
2019-10-21 14:50:26 +0200

    Multiple Generic Type Parameters, Generic Constraints, vararg, get
    Lastige materie! :/

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,22 +1,42 @@
-class LootBox<T> (item: T) {
+class LootBox<T : Loot> (vararg item: T) {
     var open = false
-    private var loot: T = item
+    private var loot: Array<out T> = item
 
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    operator fun get(index: Int): T? = loot[index].takeIf { open }
+
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
+    }
+
+    fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {
+        return lootModFunction(loot[item]).takeIf { open }
     }
 }
 
-class Fedora(val name: String, val value: Int)
+open class Loot(val value: Int)
 
-class Coin(val value: Int)
+class Fedora(val name: String, value: Int) : Loot(value)
+
+class Coin(value: Int) : Loot(value)
 
 fun main() {
-    val lootBoxOne = LootBox(Fedora("a generic-looking Fedora", 15))
+    val lootBoxOne = LootBox(Fedora("a generic-looking Fedora", 15),
+                                            Fedora("a dazzling magenta fedora", 25))
     val lootBoxTwo = LootBox(Coin(15))
 
     lootBoxOne.open = true
-    lootBoxOne.fetch()?.run {
+    lootBoxOne.fetch(1)?.run {
         println("You retrieve $name from the box!")
     }
+
+    val coin = lootBoxOne.fetch(0) {
+        Coin(it.value * 3)
+    }
+
+    coin?.let {
+        println(it.value)
+    }
+
+    val fedora = lootBoxOne[1]
+    fedora?.let { println(it.name)}
 }
